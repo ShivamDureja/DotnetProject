f184e39102ab258607e0161e7f8ae94d558d71ba
ShivamDureja <shivamdureja13@gmail.com>
2023-12-20 00:49:37 +0530

    added models

---
+++ CustomerApi/CustomerApi/Models/CustomerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerApi.Models
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-RIO0KCVU\\SQLEXPRESS;database=custdb;integrated security = true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
