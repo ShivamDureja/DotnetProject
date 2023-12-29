b97e10d884b879f8592b2011ba2988fe26a3f787
ShivamDureja <shivamdureja13@gmail.com>
2023-12-29 14:10:56 +0530

    Configured Cors Policy

--- CustomerApi/CustomerApi/Program.cs
+++ CustomerApi/CustomerApi/Program.cs
@@ -1,18 +1,40 @@
 
+using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System;
+
 namespace CustomerApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+
 
             // Add services to the container.
+            var MyAllowSpecificOrigins = "MyPolicy";
+            var builder = WebApplication.CreateBuilder(args);
+           
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<CustomerDbContext>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
 
             var app = builder.Build();
 
@@ -22,9 +44,10 @@ namespace CustomerApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseAuthorization();
-
+            
 
             app.MapControllers();
 
